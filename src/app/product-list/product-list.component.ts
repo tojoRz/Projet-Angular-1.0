6e5bbc197745e756e3adb425fe3fe66cc06a735e
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-02-05 15:26:20 +0300

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  product = {
+    name: 'iPhone X',
+    price: 798,
+    color: 'Black',
+    discount: 8.5,
+    inStock: 2,
+    pImage:'/assets/images/iphonex.png',
+  }
 
+  getDiscountedPrice(){
+    return this.product.price - (this.product.price * this.product.discount / 100)
+  }
 }

4df0a9a7a8c8c8c39e6459542bfff3cc47af3bad
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-02-09 15:25:55 +0300

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  name = 'John Smith'
+  // name = 'John Smith'
+  addToCart:number = 0;
   product = {
     name: 'iPhone X',
     price: 798,
@@ -18,5 +19,22 @@ export class ProductListComponent {
 
   getDiscountedPrice(){
     return this.product.price - (this.product.price * this.product.discount / 100)
+  }
+
+  onNameChange(event: any){
+    // this.name = event.target.value
+    //console.log(event.target.value)
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
   }
 }

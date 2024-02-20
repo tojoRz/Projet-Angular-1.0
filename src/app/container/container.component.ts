c0f70162253eff22510c7bfbffe4a9a55328427f
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-02-20 19:25:46 +0300

    Create container.component.ts

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+  // name = 'John Smith'
+  addToCart:number = 0;
+  product = {
+    name: 'iPhone X',
+    price: 798,
+    color: 'Black',
+    discount: 8.5,
+    inStock: 2,
+    pImage:'/assets/images/iphonex.png',
+  }
+
+  getDiscountedPrice(){
+    return this.product.price - (this.product.price * this.product.discount / 100)
+  }
+
+  onNameChange(event: any){
+    // this.name = event.target.value
+    //console.log(event.target.value)
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
+  }
+}

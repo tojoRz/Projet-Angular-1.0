0244a739db4cd378f48184ec9864765408a27f04
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-02-20 19:25:35 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { SearchComponent } from './product-list/search/search.component';
-import { FormsModule } from '@angular/forms'
+import { ContainerComponent } from './container/container.component';
+import { SearchComponent } from './container/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './container/product-list/product-list.component'
 
 @NgModule({
   declarations: [
@@ -18,8 +19,9 @@ import { FormsModule } from '@angular/forms'
     TopHeaderComponent,
     TopMenuComponent,
     MainMenuComponent,
-    ProductListComponent,
-    SearchComponent
+    ContainerComponent,
+    SearchComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,

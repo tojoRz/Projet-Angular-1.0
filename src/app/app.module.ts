605750a3f303245537e88730a14de13ce9485b20
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-03-15 12:06:57 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { MainMenuComponent } from './header/main-menu/main-menu.component';
 import { ContainerComponent } from './container/container.component';
 import { SearchComponent } from './container/search/search.component';
 import { FormsModule } from '@angular/forms';
-import { ProductListComponent } from './container/product-list/product-list.component'
+import { ProductListComponent } from './container/product-list/product-list.component';
+import { ProductComponent } from './container/product-list/product/product.component'
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ProductListComponent } from './container/product-list/product-list.comp
     MainMenuComponent,
     ContainerComponent,
     SearchComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,

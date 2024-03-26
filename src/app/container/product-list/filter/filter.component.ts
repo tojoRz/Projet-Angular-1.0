23f86a7ad9f9f73397dc625d5674ccba97c8d87d
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-03-26 14:03:31 +0300

    Update filter.component.ts

--- src/app/container/product-list/filter/filter.component.ts
+++ src/app/container/product-list/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -6,8 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  @Input()
   all: number = 0;
+
+  @Input()
   inStock: number = 0;
+
+  @Input()
   outOfstock: number = 0;
   
 

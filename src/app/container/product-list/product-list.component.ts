cb2d6f366cb4bbd14405481627dbc624cf7f72db
Tojo Razakanirina <toojrazakanirina01@gmail.com>
2024-03-27 12:15:19 +0300

    Update product-list.component.ts

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -57,5 +57,11 @@ export class ProductListComponent {
 
   totalProductCount = this.products.length;
   totalProductInStock = this.products.filter( p => p.is_in_inventory === true ).length;
-  totalProductOutOfStock = this.products.filter( p => p.is_in_inventory === false ).length; 
+  totalProductOutOfStock = this.products.filter( p => p.is_in_inventory === false ).length;
+
+  selectedFilterRadioButton: string = 'all';
+
+  onFilterChanged(value: string){
+    this.selectedFilterRadioButton = value;
+  } 
 }
